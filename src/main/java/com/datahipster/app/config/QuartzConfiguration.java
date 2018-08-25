64dd3b38b7416cfe57201e0f3f656c06f009d9da
Eamonn Dunne <eamonn.dunne@gmail.com>
2018-08-25 13:36:22 +0100

    kinda fixed

---
+++ src/main/java/com/datahipster/app/config/QuartzConfiguration.java
@@ -0,0 +1,23 @@
+package com.datahipster.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class QuartzConfiguration {
+
+
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(DataSource dataSource) {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setConfigLocation(new ClassPathResource("quartz.properties"));
+        schedulerFactoryBean.setDataSource(dataSource);
+        return schedulerFactoryBean;
+    }
+}
